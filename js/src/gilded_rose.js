431717118b802ee9ca8ad8ca0daf7cc08b0ffaec
Leon, Felipe <fglfgl27@gmail.com>
2023-03-22 10:36:13 -0300

    Refactor JS update_quality
    Simplify the code:
    
    -  Lowered cognitive complexity
    -  Improve readability
    -  Maintainability
    -  and extendability
    -  add proper comments

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -4,55 +4,59 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var items = [];
+const items = [];
 
 function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - (items[i].name == 'Conjured Mana Cake' ? 2 : 1);
-        }
-      }
+  for (const item of items) {
+    //"Sulfuras", being a legendary item, never has to be sold or decreases in quality
+    //and its quality is 80 and it never alters.
+    if (item.name === 'Sulfuras, Hand of Ragnaros') {
+      continue;
+    }
+
+    item.sell_in--;
+
+    if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      //"Backstage passes", like "Aged brie", increases in quality as its sell-in value approaches
+      set_quality_backstage(item);
+    } else if (item.name === 'Aged Brie') {
+      //"Aged brie", increases in quality as its sell-in value approaches
+      set_quality_with_offset(item, 1);
+    } else if (item.name === 'Conjured Mana Cake') {
+      //"Conjured" items degrade in quality twice as fast as normal items
+      set_quality_with_offset(item, -2);
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1;
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-        }
-      }
+      //"Normal" items degrade in quality
+      set_quality_with_offset(item, -1);
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - (items[i].name == 'Conjured Mana Cake' ? 2 : 1);
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].quality = Math.max(0, Math.min(50, items[i].quality));
-    }
+
+    //The quality of an item is never more than 50 or negative, unless it's "Sulfuras" which case the code will not reach this line
+    item.quality = Math.max(0, Math.min(50, item.quality));
   }
+}
+
+function set_quality_backstage(item) {
+  let qualityOffset = 1;
+
+  if (item.sell_in < 0) {
+    //"Backstage passes" quality drops to 0 after the concert
+    qualityOffset = -item.quality;
+  } else if (item.sell_in < 5) {
+    //Quality increases by 3 when there are 5 days or less
+    qualityOffset = 3;
+  } else if (item.sell_in < 10) {
+    //"Backstage passes" quality increases by 2 when there are 10 days or less
+    qualityOffset = 2;
+  }
+
+  item.quality += qualityOffset;
+}
+
+function set_quality_with_offset(item, qualityOffset) {
+  if (item.sell_in < 0) {
+    //Once the sell by date has passed, quality increases/degrades twice as fast
+    qualityOffset *= 2;
+  }
+
+  item.quality += qualityOffset;
 }
